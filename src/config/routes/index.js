08fef0f38c07faca6e082234bf51b25f30bc3f7b
Agesta Saputra <agesta.saputra@qoala.id>
2022-01-28 08:42:01 +0700

    chore: create main tag for content element

--- src/config/routes/index.js
+++ src/config/routes/index.js
@@ -3,26 +3,29 @@ import { Route, Switch, withRouter, useLocation } from "react-router-dom";
 import Navbar from "app/components/navbar";
 import Landing from "app/pages/landing";
 import Create from "app/pages/create";
+import "./styles.scss";
 
 const Routes = ({ state, dispatch }) => {
   const location = useLocation();
   return (
     <React.Suspense fallback={null}>
       <Navbar state={state} dispatch={dispatch} location={location} />
-      <Switch>
-        <Route
-          path="/create"
-          component={(props) => (
-            <Create {...props} state={state} dispatch={dispatch} />
-          )}
-        />
-        <Route
-          path="/"
-          component={(props) => (
-            <Landing {...props} state={state} dispatch={dispatch} />
-          )}
-        />
-      </Switch>
+      <main class="main-content">
+        <Switch>
+          <Route
+            path="/create"
+            component={(props) => (
+              <Create {...props} state={state} dispatch={dispatch} />
+            )}
+          />
+          <Route
+            path="/"
+            component={(props) => (
+              <Landing {...props} state={state} dispatch={dispatch} />
+            )}
+          />
+        </Switch>
+      </main>
     </React.Suspense>
   );
 };
